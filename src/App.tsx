12fb616ed5f26c7cbb349e56587db070ecbdc8f4
Chris Kang <chris.sm.kang542@gmail.com>
2026-02-03 12:03:56 +0000

    Let's start

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <button onClick={() => alert("hi")}>Click</button>
+      <h1>GIG Roster V3 WIP</h1>
+      <button onClick={() => alert("Let's go!")}>Click</button>
     </div>
   );
 }
